0f84fc0c0aba5354c0d2276478cb9730160617b7
Ajie Pangestu <paangestu@gmail.com>
2023-12-16 20:49:38 +0700

    add group name

--- api/serializers.py
+++ api/serializers.py
@@ -7,7 +7,19 @@ class UserSerializer(ModelSerializer):
 
     class Meta:
         model = User
-        fields = '__all__'
+        fields = [
+            'id', 'first_name', 'last_name',
+            'username', 'email', 'is_active',
+        ]
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data['group'] = instance.groups.all().values_list(
+            'name',
+            flat=True
+        )
+        return data
+
 
 class GroupSerializer(ModelSerializer):
 
