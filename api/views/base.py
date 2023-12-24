07ebbf8fb0eca9bd9f4bb0307b4a8d1d168d3f3c
Ajie Pangestu <paangestu@gmail.com>
2023-12-24 08:16:12 +0700

    format code

--- api/views/base.py
+++ api/views/base.py
@@ -1,5 +1,4 @@
 from rest_framework.viewsets import ModelViewSet as RestModelViewSet
-
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.exceptions import AuthenticationFailed
