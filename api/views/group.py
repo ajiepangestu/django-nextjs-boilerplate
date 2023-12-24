1d9677842e202b99e3d73b0b8142ee5f017070ef
Ajie Pangestu <paangestu@gmail.com>
2023-12-24 08:24:16 +0700

    fix models

--- api/views/group.py
+++ api/views/group.py
@@ -1,9 +1,9 @@
-from django.contrib.auth import models as django_models
+from django.contrib.auth import models
 
 from api.views.base import ModelViewSet
 from api.serializers import GroupSerializer
 
 
 class GroupView(ModelViewSet):
-    queryset = django_models.Group.objects.all().order_by('id')
+    queryset = models.Group.objects.all().order_by('id')
     serializer_class = GroupSerializer
--- api/views/user.py
+++ api/views/user.py
@@ -1,9 +1,9 @@
-from django.contrib.auth import models as django_models
+from django.contrib.auth import models
 
 from api.views.base import ModelViewSet
 from api.serializers import UserSerializer
 
 
 class UserView(ModelViewSet):
-    queryset = django_models.User.objects.all().order_by('id')
+    queryset = models.User.objects.all().order_by('id')
     serializer_class = UserSerializer
